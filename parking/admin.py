8d3c9c5f9b6e6f9b091854dc4d818fc2db6545c1
saro <hoseinisaroo@gmail.com>
2026-05-08 22:02:11 +0330

    Register models in Django admin

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from . import models 
 
-# Register your models here.
+@admin.register(models.Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.ParkingLot)
+class ParkingLotAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.ParkingSpot)
+class ParkingSpotAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Tariff)
+class TarrifAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.ParkingSession)
+class ParkingSessionAdmin(admin.ModelAdmin):
+    pass
