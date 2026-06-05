1574ecb3ed39433c29f1008b8eaa95f771f60f05
saro <hoseinisaroo@gmail.com>
2026-06-05 17:30:24 +0330

    Update admin panel for customer-based data management

--- parking/admin.py
+++ parking/admin.py
@@ -1,17 +1,49 @@
 from django.contrib import admin
-from . import models 
+from . import models
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = [
+        'id',
+        'name',
+        'owner_name',
+        'phone',
+        'email',
+        'status',
+        'is_active',
+        'created_at',
+        'approved_at',
+    ]
+    list_filter = ['status', 'is_active', 'created_at']
+    search_fields = ['name', 'owner_name', 'phone', 'email']
+    readonly_fields = ['created_at', 'approved_at']
+
+
+@admin.register(models.CustomerUser)
+class CustomerUserAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['id', 'user', 'customer', 'role', 'is_active']
+    list_filter = ['customer', 'role', 'is_active']
+    search_fields = ['user__username', 'user__email', 'customer__name']
+
 
 @admin.register(models.Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
     list_per_page = 20
-    list_display = ['id', 'plate_number_display', 'owner_name', 'owner_phone', 'type', 'color']
-    list_filter = ['type', 'color']
-    search_fields = ['plate_number', 'owner_name__istartswith', 'owner_phone']
+    list_display = ['id', 'customer', 'plate_number_display', 'owner_name', 'owner_phone', 'type', 'color']
+    list_filter = ['customer', 'type', 'color']
+    search_fields = ['plate_number', 'owner_name__istartswith', 'owner_phone', 'customer__name']
+    list_select_related = ['customer']
 
     def plate_number_display(self, obj):
-        return format_html("<span style='direction:rtl; unicode-bidi:embed;'>{}</span>", obj.plate_number)
+        return format_html(
+            "<span style='direction:rtl; unicode-bidi:embed;'>{}</span>",
+            obj.plate_number
+        )
 
     plate_number_display.short_description = "پلاک"
 
@@ -25,107 +57,128 @@ class ParkingSpotInline(admin.TabularInline):
 
 @admin.register(models.ParkingLot)
 class ParkingLotAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'total_capacity']
-    search_fields = ['name']
+    list_display = ['id', 'customer', 'name', 'total_capacity']
+    list_filter = ['customer']
+    search_fields = ['name', 'customer__name']
+    list_select_related = ['customer']
     inlines = [ParkingSpotInline]
 
 
 @admin.register(models.ParkingSpot)
 class ParkingSpotAdmin(admin.ModelAdmin):
     list_per_page = 30
-    list_display = ['id', 'parking_lot', 'code', 'level','is_occupied']
-    list_filter = ['parking_lot', 'level','is_occupied']
-    search_fields = ['parking_lot__name', 'code', 'level']
+    list_display = ['id', 'parking_lot', 'customer_display', 'code', 'level', 'is_occupied']
+    list_filter = ['parking_lot__customer', 'parking_lot', 'level', 'is_occupied']
+    search_fields = ['parking_lot__customer__name', 'parking_lot__name', 'code', 'level']
+    list_select_related = ['parking_lot', 'parking_lot__customer']
+
+    def customer_display(self, obj):
+        return obj.parking_lot.customer
+
+    customer_display.short_description = "مشتری"
 
 
 @admin.register(models.Tariff)
 class TariffAdmin(admin.ModelAdmin):
-    list_display = ['name', 'vehicle_type', 'is_active', 'first_hour_price', 'additional_hour_price', 'daily_price']
+    list_display = [
+        'customer',
+        'name',
+        'vehicle_type',
+        'is_active',
+        'first_hour_price',
+        'additional_hour_price',
+        'daily_price',
+    ]
     list_editable = ['first_hour_price', 'additional_hour_price', 'daily_price', 'is_active']
-    list_filter = ['vehicle_type', 'is_active']
-    search_fields = ['name']
+    list_filter = ['customer', 'vehicle_type', 'is_active']
+    search_fields = ['name', 'customer__name']
+    list_select_related = ['customer']
+
 
 @admin.register(models.ParkingSession)
 class ParkingSessionAdmin(admin.ModelAdmin):
     list_per_page = 15
     list_display = [
-        'id', 'entry_time', 'exit_time',
-        'total_duration_minutes', 'status',
-        'vehicle_preview', 'spot', 'calculated_fee'
+        'id',
+        'customer_display',
+        'entry_time',
+        'exit_time',
+        'total_duration_minutes',
+        'status',
+        'vehicle_preview',
+        'spot',
+        'calculated_fee',
     ]
-    list_filter = ['status', 'spot__parking_lot', 'vehicle__type']
-    search_fields = ['vehicle__plate_number', 'vehicle__owner_name', 'spot__code']
+    list_filter = ['vehicle__customer', 'status', 'spot__parking_lot', 'vehicle__type']
+    search_fields = ['vehicle__plate_number', 'vehicle__owner_name', 'spot__code', 'vehicle__customer__name']
     readonly_fields = ['total_duration_minutes', 'calculated_fee']
-    list_select_related = ['vehicle', 'spot', 'spot__parking_lot']
+    list_select_related = ['vehicle', 'vehicle__customer', 'spot', 'spot__parking_lot']
+
+    def customer_display(self, obj):
+        return obj.vehicle.customer if obj.vehicle else "-"
+
+    customer_display.short_description = "مشتری"
 
     def vehicle_preview(self, obj):
         text = obj.__str__().replace("\n", "<br>")
         return mark_safe(text)
 
     vehicle_preview.short_description = 'وسیله نقلیه'
-    
+
+
 @admin.register(models.Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_per_page = 15
-
     list_display = [
         'id',
+        'customer_display',
         'amount',
         'payment_time',
         'payment_method',
         'payment_status',
         'session',
     ]
-
     list_editable = ['payment_method', 'payment_status']
-
-    list_filter = [
-        'payment_method',
-        'payment_status',
-        'payment_time',
-    ]
-
+    list_filter = ['session__vehicle__customer', 'payment_method', 'payment_status', 'payment_time']
     search_fields = [
         'session__vehicle__plate_number',
         'session__vehicle__owner_name',
         'session__vehicle__owner_phone',
+        'session__vehicle__customer__name',
     ]
+    readonly_fields = ['amount', 'payment_time']
+    list_select_related = ['session', 'session__vehicle', 'session__vehicle__customer']
 
-    readonly_fields = [
-        'amount',
-        'payment_time',
-    ]
+    def customer_display(self, obj):
+        return obj.session.vehicle.customer if obj.session and obj.session.vehicle else "-"
 
-    list_select_related = [
-        'session',
-        'session__vehicle',
-    ]
+    customer_display.short_description = "مشتری"
+
 
 @admin.register(models.Receipt)
 class ReceiptAdmin(admin.ModelAdmin):
     list_per_page = 15
-
     list_display = [
         'receipt_number',
+        'customer_display',
         'issue_time',
         'session',
         'payment',
         'receipt_preview',
     ]
-
     list_filter = [
+        'session__vehicle__customer',
         'issue_time',
         'payment__payment_method',
         'payment__payment_status',
     ]
-
     search_fields = [
         'receipt_number',
         'session__vehicle__plate_number',
         'session__vehicle__owner_name',
+        'session__vehicle__customer__name',
         'payment__payment_method',
     ]
-
     readonly_fields = [
         'receipt_number',
         'issue_time',
@@ -133,12 +186,12 @@ class ReceiptAdmin(admin.ModelAdmin):
         'content',
         'receipt_preview',
     ]
+    list_select_related = ['payment', 'session', 'session__vehicle', 'session__vehicle__customer']
 
-    list_select_related = [
-        'payment',
-        'session',
-        'session__vehicle',
-    ]
+    def customer_display(self, obj):
+        return obj.session.vehicle.customer if obj.session and obj.session.vehicle else "-"
+
+    customer_display.short_description = "مشتری"
 
     def receipt_preview(self, obj):
         text = obj.generate_content().replace("\n", "<br>")
@@ -146,15 +199,25 @@ class ReceiptAdmin(admin.ModelAdmin):
 
     receipt_preview.short_description = "متن رسید"
 
-#History Models    
+
+# History Models
+
 @admin.register(models.ParkingSessionHistory)
 class ParkingSessionHistoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'original_id', 'vehicle', 'parking_lot', 'parking_spot', 'deleted_at')
+    list_display = ('id', 'original_id', 'customer', 'vehicle', 'parking_lot', 'parking_spot', 'deleted_at')
+    list_filter = ['customer', 'deleted_at']
+    search_fields = ['customer__name', 'vehicle__plate_number']
+
 
 @admin.register(models.PaymentHistory)
 class PaymentHistoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'original_id', 'amount', 'payment_time', 'deleted_at')
+    list_display = ('id', 'original_id', 'customer', 'amount', 'payment_time', 'deleted_at')
+    list_filter = ['customer', 'payment_status', 'deleted_at']
+    search_fields = ['customer__name']
+
 
 @admin.register(models.ReceiptHistory)
 class ReceiptHistoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'original_id', 'receipt_number', 'deleted_at')
+    list_display = ('id', 'original_id', 'customer', 'receipt_number', 'deleted_at')
+    list_filter = ['customer', 'deleted_at']
+    search_fields = ['customer__name', 'receipt_number']
