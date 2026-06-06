5d14c96aaf71c6ee33bec6a431f6f18fabcfbee1
saro <hoseinisaroo@gmail.com>
2026-06-06 20:56:31 +0330

    Update project URL configuration

--- parking_system/urls.py
+++ parking_system/urls.py
@@ -23,5 +23,5 @@ admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('parking/', include('parking.urls'))
+    path('', include('parking.urls'))
 ]
